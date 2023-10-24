b7f89f232cfbdd21af2c614a2cc09d8c2084855a
Boris Ugolev <64079766+ICrazy1337@users.noreply.github.com>
2023-10-24 21:20:42 +0300

    sync

---
+++ sync/function/users_import.sql
@@ -0,0 +1,51 @@
+CREATE OR REPLACE FUNCTION sync.users_import(_src JSONB) RETURNS JSONB
+    LANGUAGE plpgsql
+    SECURITY DEFINER
+AS
+$$
+BEGIN
+    SET TIME ZONE 'Europe/Moscow';
+
+    WITH cte AS (SELECT s.user_id,
+                        s.name,
+                        s.surname,
+                        s.phone_number,
+                        s.birth_day,
+                        s.ch_staff_id,
+                        s.ch_dt,
+                        ROW_NUMBER() over (PARTITION BY s.user_id ORDER BY s.ch_dt DESC) rn
+                 FROM JSONB_TO_RECORDSET(_src) AS s (user_id INT,
+                                                     name VARCHAR(64),
+                                                     surname VARCHAR(64),
+                                                     phone_number VARCHAR(11),
+                                                     birth_day DATE,
+                                                     ch_staff_id INT,
+                                                     ch_dt TIMESTAMPTZ))
+
+    INSERT
+    INTO humanresource.users AS sd (user_id, name, surname, phone_number, birth_day, ch_staff_id, ch_dt)
+    SELECT s.user_id, s.name, s.surname, s.phone_number, s.birth_day, s.ch_staff_id, s.ch_dt
+    FROM cte s
+    WHERE rn = 1
+    ON CONFLICT (user_id) DO UPDATE
+        SET name         = excluded.name,
+            surname      = excluded.surname,
+            phone_number = excluded.phone_number,
+            birth_day    = excluded.birth_day,
+            ch_staff_id  = excluded.ch_staff_id,
+            ch_dt        = excluded.ch_dt
+    WHERE sd.ch_dt <= excluded.ch_dt;
+
+    RETURN JSONB_BUILD_OBJECT('data', NULL);
+END
+$$;
+
+select sync.users_import('{
+  "user_id": 1,
+  "name": "Boris",
+  "surname": "Ugolev",
+  "phone_number": "+7999999999",
+  "birth_day": "25-01-2004",
+  "ch_staff_id": 2542,
+  "ch_dt": "24-10-2023"
+}')
